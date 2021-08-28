a1700db0fce0bb0786536a9640eb218016dc1cfa
litanontaru <Ghtvmthf5%t>
2021-08-28 10:38:10 +0200

    removed import

--- src/main/kotlin/org/dmg/monsterhub/data/FeatureContainerData.kt
+++ src/main/kotlin/org/dmg/monsterhub/data/FeatureContainerData.kt
@@ -1,11 +1,5 @@
 package org.dmg.monsterhub.data
 
-import org.dmg.monsterhub.data.setting.SettingObject
-import javax.persistence.CascadeType
-import javax.persistence.JoinColumn
-import javax.persistence.MappedSuperclass
-import javax.persistence.OneToMany
-
 interface FeatureContainerData {
   var features: MutableList<FeatureData>
 }
