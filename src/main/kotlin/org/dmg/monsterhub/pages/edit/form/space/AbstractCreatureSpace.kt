4974275df39fea83a0eab9315fdf6ad287d36f30
litanontaru <Ghtvmthf5%t>
2021-08-31 17:23:56 +0200

    code style

--- src/main/kotlin/org/dmg/monsterhub/pages/edit/form/space/AbstractCreatureSpace.kt
+++ src/main/kotlin/org/dmg/monsterhub/pages/edit/form/space/AbstractCreatureSpace.kt
@@ -4,7 +4,7 @@ import com.vaadin.flow.component.Component
 import org.dmg.monsterhub.data.Creature
 import org.dmg.monsterhub.pages.edit.data.ServiceLocator
 
-interface AbstractCreatureSpace: Space {
+interface AbstractCreatureSpace : Space {
   override fun support(obj: Any) = obj is Creature
 
   override fun use(anyObj: Any, locator: ServiceLocator, update: (Any, () -> Unit) -> Unit) =
