f10e3dc89e2dfc55e88fee05ab9a2ad28c19929a
litanontaru <Ghtvmthf5%t>
2021-08-31 18:08:57 +0200

    restore size profile

---
+++ src/main/kotlin/org/dmg/monsterhub/service/SizeProfile.kt
@@ -0,0 +1,27 @@
+package org.dmg.monsterhub.service
+
+data class SizeProfile(
+    val size: Int,
+    val damageModifier: Int,
+    val destructionModifier: Int,
+    val partSizeModifier: Double,
+    val weaponSizeModifier: Double,
+    val speedModifier: Double
+) {
+  fun modifyWeaponDistance(distance: Double) =
+      if (distance < 0.6) distance * partSizeModifier
+      else 0.6 * partSizeModifier + (distance - 0.6) * weaponSizeModifier
+
+  fun modifyNaturalWeaponDistance(distance: Double) = distance * partSizeModifier
+
+  companion object {
+    operator fun invoke(row: List<String>) = SizeProfile(
+        row[0].toInt(),
+        row[1].toInt(),
+        row[2].toInt(),
+        row[3].toDouble(),
+        row[3].toDouble(),
+        row[4].toDouble()
+    )
+  }
+}
---
+++ src/main/kotlin/org/dmg/monsterhub/service/SizeProfileService.kt
@@ -0,0 +1,41 @@
+package org.dmg.monsterhub.service
+
+import org.springframework.stereotype.Service
+import java.io.BufferedReader
+import java.io.IOException
+import java.io.InputStreamReader
+
+@Service
+class SizeProfileService {
+  val sizeProfiles = csv("/size.csv", 2)
+      .asSequence()
+      .map { SizeProfile(it) }
+      .groupBy { it.size }
+      .mapValues { it.value.single() }
+
+  fun get(size: Int, partsSize: Int): SizeProfile {
+    val bySize = sizeProfiles[size]!!
+    val byParts = sizeProfiles[partsSize]!!
+
+    return SizeProfile(
+        bySize.size,
+        bySize.damageModifier,
+        bySize.destructionModifier,
+        byParts.partSizeModifier,
+        bySize.weaponSizeModifier,
+        byParts.speedModifier
+    )
+  }
+}
+
+fun csv(fileName: String, columns: Int): List<List<String>> = try {
+  BufferedReader(InputStreamReader(SizeProfileService::class.java.getResourceAsStream(fileName)))
+      .use { br ->
+        br.lineSequence()
+            .map { it.split(";".toRegex()).dropLastWhile { it.isEmpty() }.toTypedArray() }
+            .map { (it.asSequence() + (it.size..columns).map { "" }).toList() }
+            .toList()
+      }
+} catch (e: IOException) {
+  emptyList()
+}
