8857e5fadb698d8f89ca5c4072c94edbd1526351
litanontaru <Ghtvmthf5%t>
2021-09-12 17:41:15 +0200

    optimized import

--- src/main/kotlin/org/dmg/monsterhub/repository/ArmorRepository.kt
+++ src/main/kotlin/org/dmg/monsterhub/repository/ArmorRepository.kt
@@ -1,7 +1,6 @@
 package org.dmg.monsterhub.repository
 
 import org.dmg.monsterhub.data.Armor
-import org.dmg.monsterhub.data.setting.Setting
 import org.springframework.stereotype.Repository
 
 @Repository
--- src/main/kotlin/org/dmg/monsterhub/repository/FolderRepository.kt
+++ src/main/kotlin/org/dmg/monsterhub/repository/FolderRepository.kt
@@ -1,7 +1,5 @@
 package org.dmg.monsterhub.repository
 
 import org.dmg.monsterhub.data.setting.Folder
-import org.dmg.monsterhub.data.setting.Setting
-import org.springframework.data.jpa.repository.JpaRepository
 
 interface FolderRepository : SettingObjectRepository<Folder>
