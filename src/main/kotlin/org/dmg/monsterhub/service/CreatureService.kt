b17f7b64e725e2e3a243c329b7c193537cca3c24
litanontaru <Ghtvmthf5%t>
2021-10-28 13:06:38 +0200

    adjusted base

--- src/main/kotlin/org/dmg/monsterhub/service/CreatureService.kt
+++ src/main/kotlin/org/dmg/monsterhub/service/CreatureService.kt
@@ -6,7 +6,7 @@ import org.springframework.stereotype.Service
 
 @Service
 object CreatureService {
-  private val BASE = listOf(-43, 0, -41, 0, -55, 0)
+  private val BASE = listOf(-42, 0, -39, 0, -59, 0)
 
   fun superiority(creature: Creature): Superiority {
     val allTraits = creature.getAllTraits()
